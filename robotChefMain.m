90e39133795bf27d8601b4a2534271dd8f2610e9
yashwinkhanna <48670096+yashwinkhanna@users.noreply.github.com>
2022-10-14 23:43:48 +1100

    IRB 910sc Baseclass (incorrect DH)

--- robotChefMain.m
+++ robotChefMain.m
@@ -14,13 +14,14 @@ hold on
 surf([-2,-2;2,2],[-2,2;-2,2],[-0.7,-0.7;-0.7,-0.7],'CData',imread('concrete.jpg'),'FaceColor','texturemap');    %Loading in concrete floor
 hold on;
 
-PlaceObject('roboticstable.ply', [-0.35,0,-0.0844]);    %Loading in table
-hold on;
+% PlaceObject('roboticstable.ply', [-0.35,0,-0.0844]);    %Loading in table
+% hold on;
+% 
+% PlaceObject('cooktop.ply', [-0.35,0,-0.0844]);    %Loading in cooktop
+% hold on;
 
-PlaceObject('cooktop.ply', [-0.35,0,-0.0844]);    %Loading in cooktop
-hold on;
-
+IRB_910sc
 
 %Create devices
-robotChef = LinearUR3(false); %robotChef - resp for actions of cooking and serving pancake
+% robotChef = LinearUR3(false); %robotChef - resp for actions of cooking and serving pancake
 %robotDispenser = 0; %robotDispenser - resp. for pouring liquids (batter, syrup)
