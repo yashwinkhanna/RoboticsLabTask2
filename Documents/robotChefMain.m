5b158b7e6f482c41aec7a5533f580524d792d90c
stefmazz <stef.mazz06@gmail.com>
2022-10-20 05:01:34 +1100

    Pretty much completed environment

--- Documents/robotChefMain.m
+++ Documents/robotChefMain.m
@@ -36,9 +36,13 @@ hold on; % To retain current plot or graphic
     % Displaying a concrete floor
         % note - the [x,x,x,x],[y,y,y,y],[z,z,z,z] are corner points so we
                  % know where to map the image
-surf([-2,-2;3.25,3.25],[-2,2;-2,2],[-0.65,-0.65;-0.65,-0.65],'CData',imread('concrete.jpg'),'FaceColor','texturemap');
+surf([-2,-2;3.25,3.25],[-2.5,2;-2.5,2],[-0.65,-0.65;-0.65,-0.65],'CData',imread('concrete.jpg'),'FaceColor','texturemap');
 
-PlaceObject('roboticstable.ply', [-0.25,0.5,-0.07]);    %Table
+surf([-2,-2;-2,-2],[-2.6,2;-2.6,2],[-0.65,-0.65;1.5,1.5],'CData',imread('wall.jpg'),'FaceColor','texturemap');
+
+surf([-2,3.25;-2,3.25],[-2.6,-2.6;-2.6,-2.6],[-0.65,-0.65;1.5,1.5],'CData',imread('wall.jpg'),'FaceColor','texturemap');
+
+%PlaceObject('roboticstable.ply', [-0.25,0.5,-0.07]);    %Table
 
 cakepos_irb = [ 0.1 ,0.49, 0.01];                       %Pancake dispense position for IRB 910
 cakepos_ur3 = [ -0.1   ,0.5,    0.12];    %[ 0.1 ,0.4, 0]        %Pancake pick up pos for LinUR3
@@ -47,10 +51,10 @@ cakepos_ur3 = [ -0.1   ,0.5,    0.12];    %[ 0.1 ,0.4, 0]        %Pancake pick u
 PlaceObject('newroboticstable.ply', [0,0,-0.0844]);
 hold on;
 
-PlaceObject('kitchenenvironment.ply', [-2,-2,-0.7]);     %Loading in kitchen environment
+PlaceObject('kitchenenvironment.ply', [-2,-2.5,-0.7]);     %Loading in kitchen environment
 hold on;
 
-PlaceObject('cookingplate.ply', [2,0,0]);     %Loading in kitchen environment
+PlaceObject('cookingplate.ply', [3,0,0]);     %Loading in kitchen environment
 hold on;
 
 PlaceObject('griddle.ply', [-0.35,-0.15,0]);     %Loading in kitchen environment
@@ -59,6 +63,83 @@ hold on;
 PlaceObject('pancake_150.ply', [-0.6,0,0.1]);     %Loading in kitchen environment
 hold on;
 
+PlaceObject('plate.ply', [-1.2,-0.4,0]);     %Loading in kitchen environment
+hold on;
+
+PlaceObject('plate.ply', [0.4,-0.5,0]);     %Loading in kitchen environment
+hold on;
+
+PlaceObject('plate.ply', [0,0,0]);     %Loading in kitchen environment
+hold on;
+
+PlaceObject('plate.ply', [0.4,0,0]);     %Loading in kitchen environment
+hold on;
+
+PlaceObject('plate.ply', [0.4,0,0.02]);     %Loading in kitchen environment
+hold on;
+
+PlaceObject('plate.ply', [0.4,0,0.04]);     %Loading in kitchen environment
+hold on;
+
+PlaceObject('plate.ply', [0.4,0,0.06]);     %Loading in kitchen environment
+hold on;
+
+PlaceObject('plate.ply', [0.4,0,0.08]);     %Loading in kitchen environment
+hold on;
+
+PlaceObject('plate.ply', [0.4,0,0.1]);     %Loading in kitchen environment
+hold on;
+
+PlaceObject('roboticshuman.ply', [-0.5,-1.25,-0.7]);     %Loading in kitchen environment
+hold on;
+
+PlaceObject('roboticsemergencystop.ply', [-1.25,-0.7,0.05]);     %Loading in kitchen environment
+hold on;
+
+PlaceObject('roboticsfireextinguisher.ply', [-1.5,-1.75,-0.7]);  %Loading fire extinguisher
+hold on;
+
+PlaceObject('roboticsfireextinguisher.ply', [2,-1.75,-0.7]);  %Loading fire extinguisher
+hold on;
+
+PlaceObject('roboticssafetyhat.ply', [-0.5,-1.25,1.3]);    %Loading a hard safety hat
+hold on;
+
+PlaceObject('roboticscamera.ply', [-0.5,0.75,0]); %Loading a camera
+hold on;
+
+PlaceObject('roboticsclock.ply', [1,-0.5,0.05]);  %loading an alarm clock
+hold on;
+
+PlaceObject('pancakemixbottle.ply', [-1.25,0.5,0]); %Loading a camera
+hold on;
+
+PlaceObject('pancakemixbottle.ply', [-1.15,0.5,0]); %Loading a camera
+hold on;
+
+PlaceObject('jammixbottle.ply', [-1.05,0.5,0]); %Loading a camera
+hold on;
+
+PlaceObject('chair.ply', [1.6,0,-0.65]); %Loading a camera
+hold on;
+
+PlaceObject('roboticsfence.ply', [-1,2,-0.7]);     %Loading in fence
+hold on;
+
+PlaceObject('roboticscone.ply', [3,1.5,-0.65]);    %Loading traffic cone
+hold on;
+
+PlaceObject('roboticscone.ply', [3,1,-0.65]);    %Loading traffic cone
+hold on;
+
+PlaceObject('roboticscone.ply', [3,0.5,-0.65]);    %Loading traffic cone
+hold on;
+
+PlaceObject('roboticscone.ply', [3,0,-0.65]);    %Loading traffic cone
+hold on;
+
+PlaceObject('roboticscone.ply', [3,-0.5,-0.65]);    %Loading traffic cone
+hold on;
 
     %Move the robots end effector to this coordinate
  irb_pos = [ -1, 0, 0.01];
