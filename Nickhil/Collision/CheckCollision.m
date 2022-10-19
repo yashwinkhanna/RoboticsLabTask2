dabfbe6f4e132796bf8d2ebeb5c20480dc1ba468
nnicx7 <nnaiker1@gmail.com>
2022-10-20 02:18:27 +1100

    collision work

---
+++ Nickhil/Collision/CheckCollision.m
@@ -0,0 +1,15 @@
+function CheckCollision(robot, sphereCenter, radius)
+% function isCollision = CheckCollision(robot, sphereCenter, radius)
+
+    pause(0.1)
+    tr = robot.fkine(robot.getpos);
+    endEffectorToCenterDist = sqrt(sum((sphereCenter-tr(1:3,4)').^2));
+    if endEffectorToCenterDist <= radius
+        disp('Oh no a collision!');
+%         isCollision = 1;
+    else
+        disp(['SAFE: End effector to sphere centre distance (', num2str(endEffectorToCenterDist), 'm) is more than the sphere radius, ' num2str(radius), 'm']);
+%         isCollision = 0;
+    end
+
+end
