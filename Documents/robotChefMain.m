ed0298afc62aa1ecd325d77594d04c8a085cb126
yashwinkhanna <48670096+yashwinkhanna@users.noreply.github.com>
2022-10-20 21:51:19 +1100

    Environment file

--- Documents/robotChefMain.m
+++ Documents/robotChefMain.m
@@ -51,99 +51,102 @@ cakepos_ur3 = [ -0.1   ,0.5,    0.12];    %[ 0.1 ,0.4, 0]        %Pancake pick u
 PlaceObject('newroboticstable.ply', [0,0,-0.0844]);
 hold on;
 
-PlaceObject('kitchenenvironment.ply', [-2,-2.5,-0.7]);     %Loading in kitchen environment
-hold on;
+enviro = 0;
+if enviro == 1
+    environmentRPC;
+end
 
-PlaceObject('cookingplate.ply', [3,0,0]);     %Loading in kitchen environment
-hold on;
 
-PlaceObject('griddle.ply', [-0.35,-0.15,0]);     %Loading in kitchen environment
-hold on;
+% PlaceObject('kitchenenvironment.ply', [-2,-2.5,-0.7]);     %Loading in kitchen environment
+% hold on;
+% 
+% PlaceObject('cookingplate.ply', [3,0,0]);     %Loading in kitchen environment
+% hold on;
+% 
+% PlaceObject('griddle.ply', [-0.35,-0.15,0]);     %Loading in kitchen environment
+% hold on;
+% 
+% PlaceObject('pancake_150.ply', [-0.6,0,0.1]);     %Loading in kitchen environment
+% hold on;
+% 
+% PlaceObject('plate.ply', [-1.2,-0.4,0]);     %Loading in kitchen environment
+% hold on;
+% 
+% PlaceObject('plate.ply', [0.4,-0.5,0]);     %Loading in kitchen environment
+% hold on;
+% 
+% PlaceObject('plate.ply', [0,0,0]);     %Loading in kitchen environment
+% hold on;
+% 
+% PlaceObject('plate.ply', [0.4,0,0]);     %Loading in kitchen environment
+% hold on;
+% 
+% PlaceObject('plate.ply', [0.4,0,0.02]);     %Loading in kitchen environment
+% hold on;
+% 
+% PlaceObject('plate.ply', [0.4,0,0.04]);     %Loading in kitchen environment
+% hold on;
+% 
+% PlaceObject('plate.ply', [0.4,0,0.06]);     %Loading in kitchen environment
+% hold on;
+% 
+% PlaceObject('plate.ply', [0.4,0,0.08]);     %Loading in kitchen environment
+% hold on;
+% 
+% PlaceObject('plate.ply', [0.4,0,0.1]);     %Loading in kitchen environment
+% hold on;
+% 
+% PlaceObject('roboticshuman.ply', [-0.5,-1.25,-0.7]);     %Loading in kitchen environment
+% hold on;
+% 
+% PlaceObject('roboticsemergencystop.ply', [-1.25,-0.7,0.05]);     %Loading in kitchen environment
+% hold on;
+% 
+% PlaceObject('roboticsfireextinguisher.ply', [-1.5,-1.75,-0.7]);  %Loading fire extinguisher
+% hold on;
+% 
+% PlaceObject('roboticsfireextinguisher.ply', [2,-1.75,-0.7]);  %Loading fire extinguisher
+% hold on;
+% 
+% PlaceObject('roboticssafetyhat.ply', [-0.5,-1.25,1.3]);    %Loading a hard safety hat
+% hold on;
+% 
+% PlaceObject('roboticscamera.ply', [-0.5,0.75,0]); %Loading a camera
+% hold on;
+% 
+% PlaceObject('roboticsclock.ply', [1,-0.5,0.05]);  %loading an alarm clock
+% hold on;
+% 
+% PlaceObject('pancakemixbottle.ply', [-1.25,0.5,0]); %Loading a camera
+% hold on;
+% 
+% PlaceObject('pancakemixbottle.ply', [-1.15,0.5,0]); %Loading a camera
+% hold on;
+% 
+% PlaceObject('jammixbottle.ply', [-1.05,0.5,0]); %Loading a camera
+% hold on;
+% 
+% PlaceObject('chair.ply', [1.6,0,-0.65]); %Loading a camera
+% hold on;
+% 
+% PlaceObject('roboticsfence.ply', [-1,2,-0.7]);     %Loading in fence
+% hold on;
+% 
+% PlaceObject('roboticscone.ply', [3,1.5,-0.65]);    %Loading traffic cone
+% hold on;
+% 
+% PlaceObject('roboticscone.ply', [3,1,-0.65]);    %Loading traffic cone
+% hold on;
+% 
+% PlaceObject('roboticscone.ply', [3,0.5,-0.65]);    %Loading traffic cone
+% hold on;
+% 
+% PlaceObject('roboticscone.ply', [3,0,-0.65]);    %Loading traffic cone
+% hold on;
+% 
+% PlaceObject('roboticscone.ply', [3,-0.5,-0.65]);    %Loading traffic cone
+% hold on;
 
-PlaceObject('pancake_150.ply', [-0.6,0,0.1]);     %Loading in kitchen environment
-hold on;
-
-PlaceObject('plate.ply', [-1.2,-0.4,0]);     %Loading in kitchen environment
-hold on;
-
-PlaceObject('plate.ply', [0.4,-0.5,0]);     %Loading in kitchen environment
-hold on;
-
-PlaceObject('plate.ply', [0,0,0]);     %Loading in kitchen environment
-hold on;
-
-PlaceObject('plate.ply', [0.4,0,0]);     %Loading in kitchen environment
-hold on;
-
-PlaceObject('plate.ply', [0.4,0,0.02]);     %Loading in kitchen environment
-hold on;
-
-PlaceObject('plate.ply', [0.4,0,0.04]);     %Loading in kitchen environment
-hold on;
-
-PlaceObject('plate.ply', [0.4,0,0.06]);     %Loading in kitchen environment
-hold on;
-
-PlaceObject('plate.ply', [0.4,0,0.08]);     %Loading in kitchen environment
-hold on;
-
-PlaceObject('plate.ply', [0.4,0,0.1]);     %Loading in kitchen environment
-hold on;
-
-PlaceObject('roboticshuman.ply', [-0.5,-1.25,-0.7]);     %Loading in kitchen environment
-hold on;
-
-PlaceObject('roboticsemergencystop.ply', [-1.25,-0.7,0.05]);     %Loading in kitchen environment
-hold on;
-
-PlaceObject('roboticsfireextinguisher.ply', [-1.5,-1.75,-0.7]);  %Loading fire extinguisher
-hold on;
-
-PlaceObject('roboticsfireextinguisher.ply', [2,-1.75,-0.7]);  %Loading fire extinguisher
-hold on;
-
-PlaceObject('roboticssafetyhat.ply', [-0.5,-1.25,1.3]);    %Loading a hard safety hat
-hold on;
-
-PlaceObject('roboticscamera.ply', [-0.5,0.75,0]); %Loading a camera
-hold on;
-
-PlaceObject('roboticsclock.ply', [1,-0.5,0.05]);  %loading an alarm clock
-hold on;
-
-PlaceObject('pancakemixbottle.ply', [-1.25,0.5,0]); %Loading a camera
-hold on;
-
-PlaceObject('pancakemixbottle.ply', [-1.15,0.5,0]); %Loading a camera
-hold on;
-
-PlaceObject('jammixbottle.ply', [-1.05,0.5,0]); %Loading a camera
-hold on;
-
-PlaceObject('chair.ply', [1.6,0,-0.65]); %Loading a camera
-hold on;
-
-PlaceObject('roboticsfence.ply', [-1,2,-0.7]);     %Loading in fence
-hold on;
-
-PlaceObject('roboticscone.ply', [3,1.5,-0.65]);    %Loading traffic cone
-hold on;
-
-PlaceObject('roboticscone.ply', [3,1,-0.65]);    %Loading traffic cone
-hold on;
-
-PlaceObject('roboticscone.ply', [3,0.5,-0.65]);    %Loading traffic cone
-hold on;
-
-PlaceObject('roboticscone.ply', [3,0,-0.65]);    %Loading traffic cone
-hold on;
-
-PlaceObject('roboticscone.ply', [3,-0.5,-0.65]);    %Loading traffic cone
-hold on;
-
-    %Move the robots end effector to this coordinate
- irb_pos = [ -1, 0, 0.01];
- ur3_pos = [ 0.1,0.4, 0];
 
 irb = IRB_910sc;                                        %Initialise the IRB and UR3 robot
 ur3 = Linear_UR3(false);
