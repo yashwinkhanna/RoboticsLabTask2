3cd068740bd94995d6d5847c7d29fcd8a458b48b
yashwinkhanna <48670096+yashwinkhanna@users.noreply.github.com>
2022-10-21 20:25:59 +1100

    plate present

--- Yashwin/finishplatepresent.m
+++ Yashwin/finishplatepresent.m
@@ -83,11 +83,11 @@ offset_z = 0.045;
 
 %% Spawn ur3 and irb at hover spot
 
-hover_pos = [-1.02, -0.53, 0.06]
+hover_pos = [-1.02, -0.53, 0.06];
 
 q00 = [-0.7400    0.0000    0.5341    1.5708    1.0367    1.5708         0];
 ur3.model.animate(q00);
-cake_now = PlaceObject('plate.ply', hover_pos);
+% cake_now = PlaceObject('plate.ply', hover_pos);
 
     
 q = zeros(1,3);         %Create a vector of initial joint angles, beginning at 0
@@ -104,27 +104,28 @@ pause(1);
     home = [-1.25, 0, 0];                
 %     qh = irb.model.ikine(transl(0.5,0.2,0) * trotx(pi), q, [1,1,1,0,0,0]);
     qh = irb.model.ikine(transl(home) * trotx(pi) * trotz(pi), q, [1,1,1,0,0,0]);
+    irb.model.animate(qh);
 
-    %jtraj creates a path between one set of joint positions (q) and a second
-    %set of joint positions (q1) using a certain amount of set increments (50)
-path = jtraj (q1,qh,50);
-
-    %Put the jtraj to action usiing for loop 
-     for i = 1:50
-        pause(0.01);
-
-        %Animate actually makes the arm move 
-        %i,: is just the saying the current ith row and all columns
-      irb.model.animate(path(i,:));
-      
-        %drawnow() displays the arm movement 
-      drawnow()
-     end
+%     %jtraj creates a path between one set of joint positions (q) and a second
+%     %set of joint positions (q1) using a certain amount of set increments (50)
+% path = jtraj (q1,qh,50);
+% 
+%     %Put the jtraj to action usiing for loop 
+%      for i = 1:50
+%         pause(0.01);
+% 
+%         %Animate actually makes the arm move 
+%         %i,: is just the saying the current ith row and all columns
+%       irb.model.animate(path(i,:));
+%       
+%         %drawnow() displays the arm movement 
+%       drawnow()
+%      end
 
 %% Move plate to safe spot
-
-rmMatrix = resolve.axial(ur3, 'x', ur3.model.fkine(q00), 0.65, 0.5);
-% cake_now = PlaceObject('pancake_150.ply', cakepos_irb);
+rmMatrix = resolve.axial(ur3, 'x', ur3.model.fkine(ur3.model.getpos), 0.65, 0.5);
+% rmMatrix = resolve.axial(ur3, 'x', ur3.model.fkine(q00), 0.65, 0.5);
+cake_now = PlaceObject('pancake_150_syrupBandplate.ply', hover_pos);
 
 spat_pos = zeros(1, 3);
 
@@ -139,12 +140,13 @@ for i = 1:resolve.steps
   spat_pos(1) = EE_pos(1, 4);
   spat_pos(2) = EE_pos(2, 4)+offset_y+0.1;
   spat_pos(3) = EE_pos(3, 4)-0.05;
-  cake_now = PlaceObject('plate.ply', spat_pos);
+  cake_now = PlaceObject('pancake_150_syrupBandplate.ply', spat_pos);
   
 end
 
 %% Move plate to table
 
+% rmMatrix = resolve.axial(ur3, 'z', ur3.model.fkine(ur3.model.getpos), -0.168, 0.5);
 rmMatrix = resolve.axial(ur3, 'z', ur3.model.fkine(ur3.model.getpos), -0.168, 0.5);
 % cake_now = PlaceObject('pancake_150.ply', cakepos_irb);
 
@@ -161,7 +163,7 @@ for i = 1:resolve.steps
   spat_pos(1) = EE_pos(1, 4);
   spat_pos(2) = EE_pos(2, 4)+offset_y+0.1;
   spat_pos(3) = EE_pos(3, 4)-0.05;
-  cake_now = PlaceObject('plate.ply', spat_pos);
+  cake_now = PlaceObject('pancake_150_syrupBandplate.ply', spat_pos);
   
 end
 
