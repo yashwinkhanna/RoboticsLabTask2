250c2a0612c7d3bafb2a4a57067ed6e4430f2c91
stefmazz <stef.mazz06@gmail.com>
2022-10-24 07:22:46 +1100

    Comitting

--- Documents/environmentRPC.m
+++ Documents/environmentRPC.m
@@ -47,23 +47,23 @@ hold on;
 % PlaceObject('roboticshuman.ply', [-0.5,-1.25,-0.7]);     %Loading in kitchen environment
 % hold on;
 
-% PlaceObject('roboticsemergencystop.ply', [-1.25,-0.7,0.05]);     %Loading in kitchen environment
-% hold on;
+PlaceObject('roboticsemergencystop.ply', [-1.25,-0.8,0.05]);     %Loading in kitchen environment
+hold on;
 
 PlaceObject('roboticsfireextinguisher.ply', [-1.5,-1.75,-0.7]);  %Loading fire extinguisher
 hold on;
 
-% PlaceObject('roboticsfireextinguisher.ply', [2,-1.75,-0.7]);  %Loading fire extinguisher
-% hold on;
+PlaceObject('roboticsfireextinguisher.ply', [2,-1.75,-0.7]);  %Loading fire extinguisher
+hold on;
 
 % PlaceObject('roboticssafetyhat.ply', [-0.5,-1.25,1.3]);    %Loading a hard safety hat
 % hold on;
 
-% PlaceObject('roboticscamera.ply', [-0.5,0.75,0]); %Loading a camera
-% hold on;
+PlaceObject('roboticscamera.ply', [-0.5,0.75,0]); %Loading a camera
+hold on;
 
-% PlaceObject('roboticsclock.ply', [1,-0.5,0.05]);  %loading an alarm clock
-% hold on;
+PlaceObject('roboticsclock.ply', [1,-0.5,0.05]);  %loading an alarm clock
+hold on;
 
 PlaceObject('pancakemixbottle.ply', [-1.25,0.5,0]); %Loading a camera
 hold on;
@@ -74,8 +74,8 @@ hold on;
 PlaceObject('jammixbottle.ply', [-1.05,0.5,0]); %Loading a camera
 hold on;
 
-% PlaceObject('chair.ply', [1.6,0,-0.65]); %Loading a camera
-% hold on;
+ PlaceObject('chair.ply', [1.6,0,-0.65]); %Loading a camera
+ hold on;
 
 % PlaceObject('roboticsfence.ply', [-1,2,-0.7]);     %Loading in fence
 % hold on;
@@ -94,3 +94,10 @@ hold on;
 
 PlaceObject('roboticscone.ply', [3,-0.5,-0.65]);    %Loading traffic cone
 hold on;
+
+trafficlightgreen = PlaceObject('trafficlightgreen.ply', [-0.75,0.75,0]);     %Loading in kitchen environment
+hold on;
+
+       trafficlightyellow = PlaceObject('trafficlightyellow.ply', [-0.75,0.75,0]);    %Loading traffic cone
+       hold on;
+       delete(trafficlightyellow);
