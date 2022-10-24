b72a8ae46ba7935b02e743076c45c219907b88cf
stefmazz <stef.mazz06@gmail.com>
2022-10-24 12:14:55 +1100

    Resume button working out??

--- Documents/isEStop.m
+++ Documents/isEStop.m
@@ -14,6 +14,7 @@
      button = estop.ButtonValuePublic;
      booleantrafficlightyellow = 0;
     % booleantrafficlightgreen = 1;
+   % booleanresumebutton = 0;
      
           if button == 1 && booleantrafficlightgreen == 1
               delete(trafficlightgreen);
@@ -21,23 +22,35 @@
           end
      
      if button == 1 
+       
       % delete(trafficlightgreen);
        booleantrafficlightyellow = 1;
        trafficlightyellow = PlaceObject('trafficlightyellow.ply', [-0.75,0.75,0]);    %Loading traffic cone
        hold on;
      end
      
+     
      while button == 1
         fprintf('STOPPED - Disengage E-Stop to continue\n');
-        pause(0.5);
-        button = estop.ButtonValuePublic;    
+        pause(2);
+        button = estop.ButtonValuePublic;   
         
+%         while button == 0 && booleanresumebutton == 0
+%          fprintf('E-Stop has been disengaged, please press resume\n')
+%          pause(1);
+%          
+%         end
+              
        % ledsvalue = 1;
      end
      
-     if booleantrafficlightyellow == 1 && button == 0
+
+     
+     
+     if booleantrafficlightyellow == 1 && button == 0 %&& booleanresumebutton == 1
      delete(trafficlightyellow);
      booleantrafficlightyellow = 0;
+     booleanresumebutton = 0;
      
       trafficlightgreen = PlaceObject('trafficlightgreen.ply', [-0.75,0.75,0]);     %Loading in kitchen environment
         hold on;
